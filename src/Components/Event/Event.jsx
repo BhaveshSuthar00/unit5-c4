36fad9d5db0f28a4108d85565d6c44bf736773b1
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-03-28 16:55:02 +0530

    add the link to meet up page

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -5,10 +5,10 @@ import { useParams } from "react-router-dom";
 import {Main } from '../Styled/Scomponents'
 export const Event = () => {
   const {id} = useParams();
+  const [valueHere, setValue] = useState(+id)
+  const [stateAre , setState] = useState(null);
   const [userDataHere, setDataUser] = useState({});
-  const [singleDd, setSingleDd] = useState({
-    subscribe : []
-  });
+  const [singleDd, setSingleDd] = useState({});
   const getSingleData = () =>{
     axios.get(`http://localhost:8080/meetups/${id}`).then((response) => {
       setSingleDd(response.data)
@@ -17,15 +17,40 @@ export const Event = () => {
   const getUser = () =>{
     let userData = JSON.parse(localStorage.getItem('userLoginDetails'));
     let current = userData[0].id;
-    console.log(current);
     axios.get(`http://localhost:8080/users/${current}`).then((response) => {
       setDataUser(response.data);
+      response.data.subscribed.map((el)=>{
+        if(+el === +valueHere){
+          setState(true);
+        } 
+        else setState(false);
+      })
     })
   }
   useEffect(()=>{
     getSingleData();
     getUser();
-  },[])
+  },[]);
+  const handleSub = (value) => {
+    let userData = JSON.parse(localStorage.getItem('userLoginDetails'));
+    let current = userData[0].id;
+      setDataUser({...userDataHere, subscribed :[...userDataHere.subscribed,value]});
+      axios.patch(`http://localhost:8080/users/${current}`, userDataHere).then((response) => {
+        setState(true);
+      }).catch((err)=> console.log(err));
+  }
+  const handleUnshb = (value) =>{
+    let userData = JSON.parse(localStorage.getItem('userLoginDetails'));
+    let current = userData[0].id;
+      let newsub = [];
+      userDataHere.subscribed.map((el)=>{
+        if(el !== value) newsub.push(el)
+      })
+      setDataUser({...userDataHere, subscribed : newsub});
+      axios.patch(`http://localhost:8080/users/${current}`, userDataHere).then((response) => {
+        setState(false);
+      }).catch((err)=> console.log(err));
+  }
   return (
     <div className="eventContainer">
       <Main>
@@ -41,10 +66,11 @@ export const Event = () => {
           <p className='location'>{singleDd.location}</p>
         </div>
       </Main>
-      {/* only one of the buttons should be visible depending on the status of subcription
-      Hint : use conditional rendering */}
-      <button className="unsubscribe">Unsubscribe</button>
-      <button className="subscribe" onClick={() => {setDataUser({...userDataHere, subscribe : [...subscribe, id]}) }}>Subscribe</button>
+      {stateAre===true ? 
+        <button className="unsubscribe" onClick={() => {handleUnshb(valueHere)}}>Subscribe</button>
+        :
+        <button className="subscribe" onClick={() => {handleSub(valueHere)} }>unSubscribe</button>
+      } 
     </div>
   );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,43 +1,55 @@
 import { Link } from "react-router-dom";
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
-
+import {Main } from '../Styled/Scomponents'
 export const Home = () => {
-  const [location, setLocation ] = useState(null);
+  const [location, setLocation] = useState(null);
   const [meetups, setMeetups] = useState([]);
+  const [subscribedData, setSubData] = useState([]);
+  const userId = JSON.parse(localStorage.getItem('userLoginDetails'));
   const getData = () => {
-    axios.get('http://localhost:8080/meetups').then((res)=>{
-    console.log(res.data);  
-    setMeetups(res.data);
-    }).catch((err)=>{
-      console.log(err);
-    })
-  }
-  useEffect(()=>{
+    axios
+      .get("http://localhost:8080/meetups")
+      .then((res) => {
+        setMeetups(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
     getData();
-  }, [])
-  if(!meetups){
-    return <h1>Loading</h1>
+  }, []);
+  if (!meetups) {
+    return <h1>Loading</h1>;
   }
   return (
     <div className="homeContainer">
-      {
-        meetups
+      {meetups
         .filter((el) => {
-          if(location !== null){
-            if(el.location.toLowerCase() === location.toLowerCase()) return true;
+          if (location !== null) {
+            if (el.location.toLowerCase() === location.toLowerCase())
+              return true;
             else return false;
-          } 
-          else if(location === '') return true;
-          else return true
-         }) // Filter on the basis of Users interests and location (both true)
-        .map((el) => {
+          } else if (location === "") return true;
+          else return true;
+        }) // Filter on the basis of Users interests and location (both true)
+        .map((singleDd) => {
           return (
-            <Link to={`/meetup/${el.id}`} key={el.id} className="events">
-              <div className='title'>
-                {el.title}
-              </div>
-
+            <Link to={`/meetup/${singleDd.id}`} key={singleDd.id} className="events">
+              <Main>
+                <div>
+                  <img src={singleDd.image} alt="" />
+                </div>
+                <div>
+                  <p className="title">{singleDd.title}</p>
+                  <p className="theme">{singleDd.theme}</p>
+                  <p className="description">{singleDd.description}</p>
+                  <p className="date">{singleDd.date}</p>
+                  <p className="time">{singleDd.time}</p>
+                  <p className="location">{singleDd.location}</p>
+                </div>
+              </Main>
               {/* add your children here (divs)
               ex : title, theme, description, date, time, location, image(optional)
               the classNames should be also : title, theme, description, date, time, location, image(optional)
@@ -49,8 +61,10 @@ export const Home = () => {
       <div className="subscribedData">
         <div>
           <select
-            value={'null'}  // add value here
-            onChange={(e) => {setLocation(e.target.value) }}
+            value={"null"} // add value here
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
           >
             <option value="null">------</option>
             <option value="bangalore">Bangalore</option>
@@ -64,17 +78,25 @@ export const Home = () => {
         <div className="subscribedEvents">
           {/* All user subcribed events should be displayed here in an ascending order of date */}
 
-          {[]
-            .map((el) => {
-              return (
-                <Link to={`/meetup/:id`} className="events">
-                  {/* Each event should have these elements/children (divs):
-                    ex : title, theme, description, date, time, location, image(optional)
-                    the classNames should be also : title, theme, description, date, time, location, image(optional) */}
-                </Link>
-              );
-            })}
-
+          {subscribedData.map((singleDd) => {
+            return (
+              <Link to={`/meetup/${singleDd.id}`} key={singleDd.id} className="events">
+              <Main>
+                <div>
+                  <img src={singleDd.image} alt="" />
+                </div>
+                <div>
+                  <p className="title">{singleDd.title}</p>
+                  <p className="theme">{singleDd.theme}</p>
+                  <p className="description">{singleDd.description}</p>
+                  <p className="date">{singleDd.date}</p>
+                  <p className="time">{singleDd.time}</p>
+                  <p className="location">{singleDd.location}</p>
+                </div>
+              </Main>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </div>
